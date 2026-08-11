77b8d825fda0eefdccb8aa45380c43578b04b57b
DevAndrés <ignacioangel671@gmail.com>
2026-08-11 16:06:19 -0400

    Update APK URL to version 1.2.0

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import { renderFeatures } from '../components/features.js';
 import { renderAbout } from '../components/about.js';
 import { renderFooter } from '../components/footer.js';
 
-const APK_URL = 'https://github.com/AndresPrograma-desing/Anubis.atm-web/releases/download/v1.1.0/anubis.atm.apk';
+const APK_URL = 'https://github.com/AndresPrograma-desing/Anubis.atm-web/releases/download/v1.2.0/anubis.atm1.2.apk';
 
 function descargarAPK() {
   window.location.href = APK_URL;
